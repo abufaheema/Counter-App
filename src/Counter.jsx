a467067987532459f0640e2bbba8313c79c6ed8d
abufaheema <oye1965@gmail.com>
2023-02-02 17:55:32 +0100

    finish project

---
+++ src/Counter.jsx
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 0,
+  };
+
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  };
+  decrement = () => {
+    this.setState({
+      count: this.state.count - 1,
+    });
+  };
+
+  reset = () => {
+    this.setState({
+      count: 0,
+    });
+  };
+
+  render() {
+    return (
+      <div className="container my-5">
+        <div className="card text-center my-5 shadow border border-4">
+          <div className="card-body">
+            <h1 className="text-primary fw-bold">Counter App</h1>
+            <div className="my-5">
+              <h2 className="my-5 text-primary fw-bold">{this.state.count}</h2>
+              <button className="btn btn-success mx-3" onClick={this.increment}>
+                Increment
+              </button>
+              <button
+                className="btn btn-danger mx-3"
+                onClick={this.decrement}
+                disabled={this.state.count === 0}
+              >
+                Decrement
+              </button>
+              <button
+                className="btn btn-secondary mx-3"
+                onClick={this.reset}
+                disabled={this.state.count === 0}
+              >
+                Reset
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Counter;
